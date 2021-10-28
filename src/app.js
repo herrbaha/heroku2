187be73b9e55f8e21de9d0bd593a6677feb96fe8
bahadir <baha_i@hotmail.com>
2021-10-28 14:00:25 +0200

    probe

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ qfil.getJsonDataFromFile('siteData.json', (siteData) => {
         res.render('info',
             {
                 ...siteData,
-                message: "Welcome to info page."
+                message: "Welcome Deneme."
             });
     });
     app.get('/', (req, res) => {
