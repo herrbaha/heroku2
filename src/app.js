0ff80b13d452e8e5d53309e3b855d8c9c4306792
bahadir <baha_i@hotmail.com>
2021-10-28 14:17:19 +0200

    probe3

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ qfil.getJsonDataFromFile('siteData.json', (siteData) => {
         res.render('info',
             {
                 ...siteData,
-                message: "Welcome to info page."
+                message: "Welcome probe."
             });
     });
     app.get('/', (req, res) => {
